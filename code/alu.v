4f05bc15913cbbec129fafd7451e85e744a9f74a
TylerRobards <tyler.robards@gmail.com>
2026-05-28 15:45:03 +1000

    20260528

--- code/alu.v
+++ code/alu.v
@@ -1,33 +1,33 @@
 module alu (
-            // Inputs
-            input wire [31:0] a,
-            input wire [31:0] b,
-            input wire [3:0]  alu_control_signal,
-            // outputs
-            output reg [31:0] result,
-            output wire       zero
-            );
-   
-   localparam ALU_ADD = 4'b0010;
-   localparam ALU_SUB = 4'b0110;
+    // Inputs
+    input  wire [31:0] a,
+    input  wire [31:0] b,
+    input  wire [ 3:0] alu_control_signal,
+    // outputs
+    output reg  [31:0] result,
+    output wire        zero
+);
 
-   always @(*) begin
-      case (alu_control_signal)
-        ALU_ADD: begin // Do add if control signal says to
-           result = a + b;
-        end
+  localparam ALU_ADD = 4'b0010;
+  localparam ALU_SUB = 4'b0110;
 
-        ALU_SUB: begin // Do sub if control signal says to
-           result = a - b;
-        end
+  always @(*) begin
+    case (alu_control_signal)
+      ALU_ADD: begin  // Do add if control signal says to
+        result = a + b;
+      end
 
-        default: begin // return zero otherwise
-           result = 32'd0;
-        end
+      ALU_SUB: begin  // Do sub if control signal says to
+        result = a - b;
+      end
 
-      endcase
-   end
+      default: begin  // return zero otherwise
+        result = 32'd0;
+      end
 
-   assign zero = (result == 32'd0); // zero flag
+    endcase
+  end
+
+  assign zero = (result == 32'd0);  // zero flag
 
 endmodule
--- code/instruction_mem.v
+++ code/instruction_mem.v
@@ -1,17 +1,17 @@
 module instruction_memory (
-                           input wire [31:0]  addr,
-                           output wire [31:0] instruction
-                           );
+    input  wire [31:0] addr,
+    output wire [31:0] instruction
+);
 
-   // 256 words of instruction memory
-   reg [31:0] memory [0:255];
+  // 256 words of instruction memory
+  reg [31:0] memory[0:255];
 
-   initial begin
-      // Load the code
-      $readmemh("program.hex", memory);
-   end
+  initial begin
+    // Load the code
+    $readmemh("program.hex", memory);
+  end
 
-   // Word aligned addressing, divide by 4
-   assign instruction = memory[addr >> 2];
+  // Word aligned addressing, divide by 4
+  assign instruction = memory[addr>>2];
 
 endmodule
